801a96895e077b840d1001e8b645dfd35dc14ee2
Vansh Dhall <vanshdhall918@gmail.com>
2025-09-02 22:24:03 +0530

    Exception or logging files Creation

---
+++ Network Security/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements() -> List[str]:
+    """ This function will return the list of requirements"""
+
+    requirements_list: List[str] = []
+    try:
+        with open('requirements.txt','r') as file:
+            # Read the requirements from the file
+            lines = file.readlines()
+            # Process each line
+            for line in lines:
+                requirement = line.strip()
+                # ignore empty lines and -e.
+                if requirement and requirement != '-e .':
+                    requirements_list.append(requirement)
+
+    except FileNotFoundError:
+        print("The requirements.txt file was not found.")
+
+    return requirements_list
+
+print(get_requirements())
