bf685fcb51a5ebe9f35465c89b35a551813fa69e
Vansh Dhall <vanshdhall918@gmail.com>
2025-09-05 23:20:56 +0530

    Data Transformation Commit

--- Network Security/main.py
+++ Network Security/main.py
@@ -1,5 +1,6 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation,DataValidationConfig
+from networksecurity.components.data_transformation import DataTransformation,DataTransformationConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
@@ -20,8 +21,12 @@ if __name__ == "__main__":
        data_validation_artifact = data_validation.initiate_data_validation()
        logging.info('Data Validation Completed')
        print(data_validation_artifact)
-       
-
+       data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+       logging.info('Data Transformation Started')
+       data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
+       data_transformation_artifact = data_transformation.initiate_data_transformation()
+       logging.info("Data Transformation Completed")
+       print(data_transformation_artifact)
 
 
     except Exception as e:
